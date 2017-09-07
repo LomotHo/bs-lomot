ff6e6e7979d6f6cf4f67b293dfc24379defd18f4
Lomot <lomot@qq.com>
2017-09-07 08:51:08 +0800

    Revert "Revert "	modified:   controller/api.js""
    This reverts commit 2d49ffe284f69ecee5935e796a9c0268a224d84a.

--- action/index.js
+++
@@ -1,7 +0,0 @@
-const article = require('./article.js');
-const user = require('./user.js');
-
-module.exports = {
-    "article": article,
-    "user": user
-};
--- model/Article.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db');
-
-module.exports = db.defineModel('article', {
-    userId: db.STRING(50),
-    title: db.STRING(100),
-    title2: db.STRING(100),
-    content: db.STRING(5000)
-});
